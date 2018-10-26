8eb861b412a6ffda898196911abf8bc3c9cde40c
Mark Barlescu <barlescu92@gmail.com>
2018-10-26 20:34:03 +0000

    first step

--- exportPractice.js
+++ exportPractice.js
@@ -3,10 +3,7 @@ var storage = []
 module.exports = {
   store: function (a){
     storage.push(a);
-    return storage;
   },
-  i: 1,
-  storage: "dogs are the best people",
 };
 
 // console.log(storage)
