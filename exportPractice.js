56c2f3df63e35159a7c2f83e107f5da80038065f
Mark Barlescu <barlescu92@gmail.com>
2018-10-26 21:16:21 +0000

    working on 2nd step

--- exportPractice.js
+++ exportPractice.js
@@ -1,10 +1,23 @@
-var storage = []
+var storage = [3,5,11,7,22]
+// console.log(storage)
+var sort = storage.sort(function(a,b){
+  return a-b
+});
+
+console.log(sort)
 
 module.exports = {
   store: function (a){
     storage.push(a);
+    // return storage;
   },
-};
+  sort: function(a,b){
+      // return a-b;
+    }
+  }
+// console.log(storage)
+
+
 
 // console.log(storage)
 // store(1);
